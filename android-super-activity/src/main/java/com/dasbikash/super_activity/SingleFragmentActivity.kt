c35195572cdbeb1c46e2794b27f4069659277678
Bikash Das <bikashdaseee@gmail.com>
2020-02-22 23:06:06 +0600

    'SingleFragmentActivity' finalized.

--- android-super-activity/src/main/java/com/dasbikash/super_activity/SingleFragmentActivity.kt
+++ android-super-activity/src/main/java/com/dasbikash/super_activity/SingleFragmentActivity.kt
@@ -7,6 +7,7 @@ import androidx.annotation.CallSuper
 import androidx.annotation.IdRes
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import com.dasbikash.android_snackbar_utils.SnackBarUtils
 import kotlinx.coroutines.GlobalScope
@@ -18,7 +19,7 @@ abstract class SingleFragmentActivity : AppCompatActivity(){
 
     private val childArgumentStack = Stack<Bundle>()
 
-    private var fragmentTransactionOnGoing = AtomicBoolean(false)
+    private val fragmentTransactionOnGoing = AtomicBoolean(false)
 
     @CallSuper
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,7 +40,7 @@ abstract class SingleFragmentActivity : AppCompatActivity(){
      * Trigger a navigation to the specified fragment, optionally adding a transaction to the back
      * stack to make this navigation reversible.
      */
-    private fun navigateTo(fragment: ChildFragment) {
+    private fun navigateTo(fragment: Fragment) {
         val fragmentTransaction =
             supportFragmentManager
             .beginTransaction()
@@ -51,7 +52,7 @@ abstract class SingleFragmentActivity : AppCompatActivity(){
             }
         GlobalScope.launch {
             if (fragmentTransactionOnGoing.get()){
-                SnackBarUtils.showShortSnack(this@SingleFragmentActivity,WAIT_MESSAGE)
+                SnackBarUtils.showShortSnack(this@SingleFragmentActivity,waitMessage)
                 return@launch
             }
             fragmentTransactionOnGoing.getAndSet(true)
@@ -63,21 +64,19 @@ abstract class SingleFragmentActivity : AppCompatActivity(){
         }
     }
 
-    fun addFragment(fragment: ChildFragment,clearFragmentStack:Boolean = false) {
+    fun addFragment(fragment: Fragment,clearFragmentStack:Boolean = false) {
         if (clearFragmentStack){
             clearFragmentStack()
         }else{
             supportFragmentManager.findFragmentById(getLoneFrameId())?.apply {
-                if (this is ChildFragment){
-                    val bundle:Bundle
-                    if (this.arguments !=null){
-                        bundle = this.arguments!!
-                    }else{
-                        bundle = Bundle()
-                    }
-                    ChildFragment.addTypeTag(bundle, this)
-                    childArgumentStack.push(bundle)
+                val bundle:Bundle
+                if (this.arguments !=null){
+                    bundle = this.arguments!!
+                }else{
+                    bundle = Bundle()
                 }
+                addTypeTag(bundle, this)
+                childArgumentStack.push(bundle)
             }
         }
         navigateTo(fragment)
@@ -92,10 +91,24 @@ abstract class SingleFragmentActivity : AppCompatActivity(){
     @CallSuper
     override fun onBackPressed() {
         if (childArgumentStack.isNotEmpty()){
-            val arguments = childArgumentStack.pop()
-            navigateTo(ChildFragment.getInstance(arguments))
+            do {
+                val arguments = childArgumentStack.pop()
+                getInstance<Fragment>(arguments)?.let {
+                    navigateTo(it)
+                    return
+                }
+            }while (childArgumentStack.isNotEmpty())
+            super.onBackPressed()
         }else {
             super.onBackPressed()
+        }
+    }
+
+    private var waitMessage = "Please wait..."
+
+    protected fun setWaitMessage(waitMessage:String){
+        if (waitMessage.isNotBlank()) {
+            this.waitMessage = waitMessage.trim()
         }
     }
 
@@ -103,10 +116,28 @@ abstract class SingleFragmentActivity : AppCompatActivity(){
     protected abstract fun getLoneFrameId():Int
     @LayoutRes
     protected abstract fun getLayoutID(): Int
-    protected abstract fun getDefaultFragment():ChildFragment
+    protected abstract fun getDefaultFragment():Fragment
 
     companion object{
-        private const val WAIT_MESSAGE = "Please wait..."
+
+        private const val ARG_FRAGMENT_TYPE =
+            "com.dasbikash.super_activity.SingleFragmentActivity.ARG_FRAGMENT_TYPE"
+
+        private fun <T:Fragment> addTypeTag(arguments:Bundle, fragment: T){
+            arguments.putSerializable(ARG_FRAGMENT_TYPE,fragment.javaClass.canonicalName!!)
+        }
+
+        private fun <T:Fragment> getInstance(bundle: Bundle):T?{
+            try {
+                val typeString = bundle.getSerializable(ARG_FRAGMENT_TYPE) as String
+                val fragment = Class.forName(typeString).newInstance() as T
+                fragment.arguments = bundle
+                return fragment
+            }catch (ex:Throwable){
+                ex.printStackTrace()
+                return null
+            }
+        }
     }
 }
 
