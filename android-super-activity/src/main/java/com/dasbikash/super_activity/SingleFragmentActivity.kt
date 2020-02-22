489aaabd9d6b8acefc7b6519ea6c9ca9400ec7ac
Bikash Das <das.bikash.asl@gmail.com>
2020-02-22 18:58:36 +0600

    Fragment transection delay handled.

--- android-super-activity/src/main/java/com/dasbikash/super_activity/SingleFragmentActivity.kt
+++ android-super-activity/src/main/java/com/dasbikash/super_activity/SingleFragmentActivity.kt
@@ -1,15 +1,24 @@
 package com.dasbikash.super_activity
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.annotation.CallSuper
 import androidx.annotation.IdRes
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import com.dasbikash.android_snackbar_utils.SnackBarUtils
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import java.util.*
+import java.util.concurrent.atomic.AtomicBoolean
 
 abstract class SingleFragmentActivity : AppCompatActivity(){
 
     private val childArgumentStack = Stack<Bundle>()
+
+    private var fragmentTransactionOnGoing = AtomicBoolean(false)
 
     @CallSuper
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,8 +39,27 @@ abstract class SingleFragmentActivity : AppCompatActivity(){
      * Trigger a navigation to the specified fragment, optionally adding a transaction to the back
      * stack to make this navigation reversible.
      */
-    private fun navigateTo(fragment: ChildFragment)
-            = supportFragmentManager.beginTransaction().replace(getLoneFrameId(), fragment).commit()
+    private fun navigateTo(fragment: ChildFragment) {
+        val fragmentTransaction =
+            supportFragmentManager
+            .beginTransaction()
+            .replace(getLoneFrameId(), fragment)
+            .runOnCommit {
+                GlobalScope.launch {
+                    fragmentTransactionOnGoing.getAndSet(false)
+                }
+            }
+        GlobalScope.launch {
+            if (fragmentTransactionOnGoing.get()){
+                SnackBarUtils.showShortSnack(this@SingleFragmentActivity,WAIT_MESSAGE)
+                return@launch
+            }
+            fragmentTransactionOnGoing.getAndSet(true)
+            runOnUiThread {
+                fragmentTransaction.commit()
+            }
+        }
+    }
 
     fun addFragment(fragment: ChildFragment,clearFragmentStack:Boolean = false) {
         if (clearFragmentStack){
@@ -74,4 +102,18 @@ abstract class SingleFragmentActivity : AppCompatActivity(){
     @LayoutRes
     protected abstract fun getLayoutID(): Int
     protected abstract fun getDefaultFragment():ChildFragment
+
+    companion object{
+        private const val WAIT_MESSAGE = "Please wait..."
+    }
+}
+
+internal fun AppCompatActivity.runIfActive(task:()->Any?){
+    if (lifecycle.currentState == Lifecycle.State.RESUMED) {
+        task()
+    }
+}
+
+internal fun runOnMainThread(task: () -> Any?,delayMs:Long=0L){
+    Handler(Looper.getMainLooper()).postDelayed( { task() },delayMs)
 }
